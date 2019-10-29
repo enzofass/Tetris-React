c1e0dce858b7086f077945f72789bf7f79662b29
enzofass <zoefassioli@gmail.com>
2019-10-29 12:10:46 -0400

    Updates

---
+++ precache-manifest.15e095fb0ed31b3aa75673d89b5b8a9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8bd4b5dfa92f4a03ef42ac96186234",
+    "url": "/Tetris-React/index.html"
+  },
+  {
+    "revision": "c5301d6042d7eb6b634e",
+    "url": "/Tetris-React/static/css/main.21d0f594.chunk.css"
+  },
+  {
+    "revision": "4a9997c7cb4f59c06f49",
+    "url": "/Tetris-React/static/js/2.7e5bb79d.chunk.js"
+  },
+  {
+    "revision": "c5301d6042d7eb6b634e",
+    "url": "/Tetris-React/static/js/main.cd1280d4.chunk.js"
+  },
+  {
+    "revision": "22594928916c04561284",
+    "url": "/Tetris-React/static/js/runtime-main.4801d6a0.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris-React/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Tetris-React/static/media/bg.2810fe6b.png"
+  }
+]);
